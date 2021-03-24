9163e1909b84480e68e7f4a894c5734f52af8a16
Pavel Makhov <pmakhov@solutionsmedias360.com>
2021-03-24 15:40:44 -0400

    init

---
+++ init.lua
@@ -0,0 +1,198 @@
+local obj = {}
+obj.__index = obj
+
+-- Metadata
+obj.name = "spotify-search-and-play"
+obj.version = "1.0"
+obj.author = "Pavel Makhov"
+obj.homepage = ""
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+obj.refresh_token_req_header = nil
+obj.token = nil
+obj.token_expires_at = nil
+obj.choose = nil
+obj.delayed_timer = nil
+obj.item_type_to_search = 'track'
+obj.iconPath = hs.spoons.resourcePath("icons")
+
+local user_icon = hs.styledtext.new(' ', { font = {name = 'feather', size = 12 }})
+local number_icon = hs.styledtext.new(' ', { font = {name = 'feather', size = 12 }})
+
+
+
+function obj:refresh_token()
+    local status, body = hs.http.post('https://accounts.spotify.com/api/token', 
+        'grant_type=client_credentials', 
+        { Authorization = 'Basic ' .. self.refresh_token_req_header})
+    
+    -- print('Refresh token status: ' .. status)
+    -- print('Refresh token body: ' .. body)
+
+    local response = hs.json.decode(body)
+    self.token = response.access_token
+    self.token_expires_at = os.time() + response.expires_in
+end
+
+
+function obj:get_auth_header()
+    if self.token == nil or os.difftime(self.token_expires_at, os.time()) < 0 then
+        self:refresh_token()
+    end
+    
+    return {Authorization = 'Bearer ' ..self.token}
+end
+
+
+function obj:build_list(body)
+    local result = {}
+
+    if self.item_type_to_search == 'track' then
+        local tracks = hs.json.decode(body).tracks.items
+        for _, track in ipairs(tracks) do
+            table.insert(result, {
+                image = hs.image.imageFromURL(track.album.images[3].url),
+                text = track.name,
+                subText = track.album.name .. '   ' .. track.artists[1].name,
+                spotify_id = track.uri
+            })
+        end
+    elseif self.item_type_to_search == 'artist' then
+        local artists = hs.json.decode(body).artists.items
+        for _, artist in ipairs(artists) do
+            local image
+            if #artist.images == 3 then image = hs.image.imageFromURL(artist.images[1].url)
+            elseif #artist.images == 0 then image = nil 
+            else hs.image.imageFromURL(artist.images[1].url) 
+            end
+
+            table.insert(result, {
+                image = image,
+                text = artist.name,
+                spotify_id = artist.uri
+            })
+        end
+    elseif self.item_type_to_search == 'playlist' then
+        local playlists = hs.json.decode(body).playlists.items
+        for _, playlist in ipairs(playlists) do
+            table.insert(result, {
+                image = hs.image.imageFromURL(playlist.images[1].url),
+                text = playlist.name,
+                subText = 'by ' .. playlist.owner.display_name,
+                spotify_id = playlist.uri
+            })
+        end
+    elseif self.item_type_to_search == 'album' then
+        local albums = hs.json.decode(body).albums.items
+        for _, album in ipairs(albums) do
+            table.insert(result, {
+                image = hs.image.imageFromURL(album.images[3].url),
+                text = album.name,
+                subText = user_icon .. album.artists[1].name,
+                spotify_id = album.uri
+            })
+        end
+    end
+
+    return result
+end
+
+
+function obj:queryChangedCallbackDelayed()
+    self.delayer_timer:start()
+end
+
+
+function obj:queryChangedCallback()
+    local str = self.chooser:query()
+    if string.len(str) < 3 then return self.chooser end
+
+    local res = {}
+    local jira_url = 'https://api.spotify.com/v1/search?q=' .. hs.http.encodeForQuery(str) .. '&type=' .. self.item_type_to_search
+    status, body = hs.http.get(jira_url, self:get_auth_header())
+    -- print(status)
+    -- print(body)
+    local res = self:build_list(body)
+    -- local issues = hs.json.decode(body).tracks.items
+    -- for _, issue in ipairs(issues) do
+    --     -- print(issue.name)
+    --     table.insert(res, {
+    --         image = hs.image.imageFromURL(issue.album.images[3].url),
+    --       text = issue.name,
+    --       subText = issue.album.name .. '   ' .. issue.artists[1].name,
+    --       spotify_id = issue.uri
+    --     })
+    -- end
+
+    return self.chooser:choices(res)
+end
+
+
+function obj:init()
+    self.chooser = hs.chooser.new(function(item)
+        if item ~= nil then
+            hs.spotify.playTrack(item.spotify_id)
+        end
+    end)
+
+    self.chooser:searchSubText(true)
+    -- obj.chooser:choices({})
+    
+    self.chooser:queryChangedCallback(hs.fnutils.partial(self.queryChangedCallbackDelayed, self))
+    self.chooser:hideCallback(function() 
+        self.chooser:choices(nil) 
+        self.chooser:query(nil)
+    end)
+
+    self.delayer_timer = hs.timer.delayed.new(1, function() self:queryChangedCallback() end)
+
+    t = require("hs.webview.toolbar")
+    a = t.new("myConsole", {
+            { id = "album", selectable = true, image = hs.image.imageFromPath(obj.iconPath ..  '/compact-disc.png'):template(true)},
+            -- { id = "NSToolbarSpaceItem" },
+            { id = "artist", selectable = true, image = hs.image.imageFromPath(obj.iconPath ..  '/microphone.png'):template(true)},
+            -- { id = "NSToolbarSpaceItem" },
+            { id = "playlist", selectable = true, image = hs.image.imageFromPath(obj.iconPath ..  '/playlist.png'):template(true)},
+            -- { id = "NSToolbarSpaceItem" },
+            { id = "track", selectable = true, image = hs.image.imageFromPath(obj.iconPath ..  '/musical-note.png'):template(true)},
+            -- { id = "notShown", default = false, image = hs.image.imageFromName("NSBonjour") },
+            -- { id = "navGroup", label = "Navigation", groupMembers = { "navLeft", "navRight" }},
+            -- { id = "navLeft", image = hs.image.imageFromName("NSGoLeftTemplate"), allowedAlone = false },
+            -- { id = "navRight", image = hs.image.imageFromName("NSGoRightTemplate"), allowedAlone = false },
+            -- { id = "NSToolbarFlexibleSpaceItem" },
+            -- { id = "cust", label = "customize", fn = function(t, w, i) t:customizePanel() end, image = hs.image.imageFromName("NSAdvanced") }
+        }):canCustomize(true)
+          :autosaves(true)
+          :selectedItem("track")
+          :sizeMode("small")
+          :setCallback(function(toolbar, chooser, identifier)
+                            self.item_type_to_search = identifier
+                            self:queryChangedCallback()
+                       end)
+    
+    t.attachToolbar(a)
+
+    self.chooser:attachedToolbar(a)
+
+
+end
+
+
+function obj:setup(args)
+    self.refresh_token_req_header = hs.base64.encode(args.client_id .. ':' .. args.secret)
+    -- print('refresh_token_req_header = ' .. self.refresh_token_req_header)
+end
+
+function obj:show()
+    self.chooser:show()
+end
+
+function obj:bindHotkeys(mapping)
+    local spec = {
+        show = hs.fnutils.partial(self.show, self),
+      }
+      hs.spoons.bindHotkeysToSpec(spec, mapping)
+      return self
+end
+
+return obj
